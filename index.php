4fd7356c4ea676821a6481ef9b35cd61845a1bd6
evgen_lesnoy <info@tportfolio.ru>
2015-03-11 09:59:44 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Автошкола!</title>
+  <link href="styles.css" type="text/css" rel="stylesheet" />
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="wrapper-ie">
+	<div class="wrapper">
+		<div class="header">
+			<div><a href="#"><img src="/images/logo.png"></a></div>
+			<div class="phone"><i class="fa fa-phone"></i> (925) 449-1238</div>
+			<div class="address">Москва, Варшавское шоссе д.39. <br>Первый этаж. Офис 311</div>
+			<div class="slogan">Автошкола №1</div>
+		</div>
+		<div class="menu-main">
+			<ul>
+				<li><a href="#">О компании</a></li>
+				<li>|</li>
+				<li><a href="#">Акции</a></li>
+				<li>|</li>
+				<li><a href="#">Цены и условия</a></li>
+				<li>|</li>
+				<li><a href="#">Фото/Видео</a></li>
+				<li>|</li>
+				<li><a href="#">Вакансии</a></li>
+				<li>|</li>
+				<li><a href="#">Справочник</a></li>
+				<li>|</li>
+				<li><a href="#">Истории студентов</a></li>
+				<li>|</li>
+				<li><a href="#">Экстрим</a></li>
+				<li>|</li>
+				<li><a href="#">Автосалон</a></li>
+				<li>|</li>
+				<li><a href="#">Вебинары</a></li>
+				<li>|</li>
+				<li><a href="#">Еще</a></li>
+			</ul>
+		</div>
+		<div class="content-main">
+        	<div class="content-main-znak"></div>
+        	<div class="content-groups-wrapper">
+           		<div class="content-groups">
+           			<div class="content-groups-ugolok"></div>
+                	<span>ФОРМИРОВАНИЕ ГРУПП НА</span>
+                	<span class="mouth">ИЮЛЬ <small>[12 человек]</small></span>
+                	<ul>
+                		<li><a href="#">утренняя</a></li>
+                		<li><a href="#">дневная</a></li>
+                		<li><a href="#">вечерняя</a></li>
+                		<li><a href="#">группа выходного дня</a></li>
+                	</ul>
+           		</div>
+        	</div>
+        	<div class="content-cabinet-wrapper">
+        		<div class="content-cabinet">
+        			<span>Вход в личный кабинет</span>
+        			<p><i class="fa fa-user fa-1x"></i><input type="text" onclick="this.value=''" name="login" value="логин"></p>
+        			<p><i class="fa fa-lock"></i><input type="password" onclick="this.value=''" name="password" value="пароль"></p>
+        			<p><a href="#">Регистрация</a> | <a href="#">Забыли пароль?</a></p>
+        		</div>
+        	</div>
+		</div>
+		<div class="blocks">
+    		<div class="block">
+	        	<div class="head"><a href="#"><i class="fa fa-thumb-tack"></i> Наши филиалы</a></div>
+	        	<div class="content">
+	        		<a href="#"><img src="/images/map-moscow.jpg"/></a>
+	       		</div>
+    		</div>
+    		<div class="block">
+	        	<div class="head"><a href="#"><i class="fa fa-camera-retro"></i> Наши фотоотчеты</a></div>
+	        	<div class="content">
+	        		<div class="gallery">
+	        			<div class="gallery-item"><a href="#"><img src="/images/gallery-item-1.jpg"/></a></div>
+	        			<div class="gallery-item"><a href="#"><img src="/images/gallery-item-2.jpg"/></a></div>
+	        			<div class="gallery-item"><a href="#"><img src="/images/gallery-item-3.jpg"/></a></div>
+	        			<div class="gallery-item"><a href="#"><img src="/images/gallery-item-4.jpg"/></a></div>
+	        		</div>
+	        	</div>
+    		</div>
+    		<div class="block">
+	        	<div class="head"><a href="#"><i class="fa fa-bullhorn"></i> Отзывы</a></div>
+	        	<div class="content">
+	        		<div class="reviews">
+	        			<div class="reviews-item"><span>Анна:</span><a href="#">Отличная автошкола, грамотный подход, много толковых инструкторов</a></div>
+	        			<div class="reviews-item"><span>Владимир:</span><a href="#">Отличная автошкола, грамотный подход, много толковых инструкторов</a></div>
+	        			<div class="reviews-item"><span>Андрей:</span><a href="#">Отличная автошкола, грамотный подход, много толковых инструкторов</a></div>
+	        		</div>
+	        	</div>
+    		</div>
+    		<div class="block">
+	        	<div class="head"><a href="#"><i class="fa fa-car"></i> Наши инструкторы</a></div>
+	        	<div class="content">
+	        		<div class="instructors">
+	        			<div class="instructors-item">
+	        				<a href="#"><img src="/images/instructor-item-1.jpg"/></a>
+	        				<a href="#">Чернышев Максим Львович</a>
+	        				<span>Рейтинг: <span>9,5</span></span>
+	        			</div>
+	        			<div class="instructors-item">
+	        				<a href="#"><img src="/images/instructor-item-2.jpg"/></a>
+	        				<a href="#">Сычев Николай Сергеевич</a>
+	        				<span>Рейтинг: <span>9,5</span></span>
+	        			</div>
+	        			<div class="instructors-item">
+	        				<a href="#"><img src="/images/instructor-item-3.jpg"/></a>
+	        				<a href="#">Сычев Николай Сергеевич</a>
+	        				<span>Рейтинг: <span>9,5</span></span>
+	        			</div>
+	        		</div>
+	        	</div>
+    		</div>
+		</div>
+		<div class="content-info-wrapper">
+			<div class="content-info">
+    			<h1><i class="fa fa-graduation-cap"></i> АВТОШКОЛА</h1>
+    			<p>Леди, стремящиеся приобщиться к ряду автолюбителей. Будущие покорительницы автомобильных дорог, перекрестков и автомагистралей. Для Вас мы открыли женскую автошколу. Что это значит?Теперь есть место, где вас профессионально научат водить автомобиль. Без грубого обращения, без ругани, без лишней критики. Несмотря на то, что с каждым годом становится больше женщин - водителей, среди некоторых мужчин женщина за рулем по-прежнему вызывает иронию, что, безусловно, несправедливо. Можно ли повлиять на этот стереотип и его разрушить? Можно. Понимая, что женщины осваивают дорогу иначе, нежели мужчины, мы предлагаем свой подход к обучению. Теперь именно женщина расскажет вам как нужно водить автомобиль. Она расскажет о тех тонкостях, на которые, как правило не обращают внимания мужчины. Каждый автоинструктор в нашей автошколе - не просто женщина - водитель, а реальный профессионал своего дела, инструктор по вождению с много</p>
+			</div>
+		</div>
+		<div class="footer">
+			<div>Автошкола Новая <br> &copy; Все права зищищены  2014 г.</div>
+			<div><a target="_blank" href="#"><img src="/images/ch1.jpg"></a> <a target="_blank" href="#"><img src="/images/ch2.jpg"></a></div>
+			<div class="site-create"><a target="_blank" href="http://aleksa.ru">Разработка сайта - студия дизайна Алекса</a></div>
+			<img class="qr" src="/images/qr.jpg"/>
+		</div>
+		<div class="menu-footer">
+			<ul>
+				<li><a href="#">О компании</a></li>
+				<li>|</li>
+				<li><a href="#">Акции</a></li>
+				<li>|</li>
+				<li><a href="#">Цены и условия</a></li>
+				<li>|</li>
+				<li><a href="#">Фото/Видео</a></li>
+				<li>|</li>
+				<li><a href="#">Вакансии</a></li>
+				<li>|</li>
+				<li><a href="#">Справочник</a></li>
+				<li>|</li>
+				<li><a href="#">Истории студентов</a></li>
+				<li>|</li>
+				<li><a href="#">Экстрим</a></li>
+				<li>|</li>
+				<li><a href="#">Автосалон</a></li>
+				<li>|</li>
+				<li><a href="#">Вебинары</a></li>
+				<li>|</li>
+				<li><a href="#">Еще</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+</body>
+
+</html>
